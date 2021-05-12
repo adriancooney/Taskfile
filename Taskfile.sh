71e600e995983ddfc0b4ebfd667720cc8b33e72b
Sander van Dragt <sander@vandragt.com>
2021-05-12 13:57:45 +0100

    Edit existing .Taskfile or the task runner if none exist.

--- Taskfile.sh
+++ Taskfile.sh
@@ -2,28 +2,37 @@
 PATH=./node_modules/.bin:$PATH
 
 edit() {
-    $EDITOR "$0"
+    # Edit existing .Taskfile or this script.
+    FILE=".Taskfile"
+    if [ ! -f "$FILE" ]; then
+        FILE="$0"
+    fi
+    echo "Editing $FILE..."
+    $EDITOR "$FILE"
 }
 
 help() {
+    # List all tasks
     echo "$0 <task> <args>"
     echo "Tasks:"
     compgen -A function | grep -v "_" | cat -n
 }
 
-# Load parent .Taskfile files
-IFS='/' read -ra dirs <<< "$(pwd)"
-dir=''
-for i in "${dirs[@]}"
-do
-	[ -z "$i" ] && continue
-	dir="${dir}/${i}"   
-    f="${dir}/.Taskfile"
-    if [ -f $f ]; then
-      echo "Using: $f"; source "$f"
-    fi
-done
-echo
+_main() {
+    # Load parent .Taskfile files
+    IFS='/' read -ra dirs <<< "$(pwd)"
+    dir=''
+    for i in "${dirs[@]}"
+    do
+        [ -z "$i" ] && continue
+        dir="${dir}/${i}"   
+        f="${dir}/.Taskfile"
+        if [ -f $f ]; then
+          echo "Using: $f"; source "$f"
+        fi
+    done
+    echo
 
-TIMEFORMAT="Task completed in %3lR"
-time ${@:-help}
+    ${@:-help}    
+}
+_main $@
